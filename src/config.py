96a646532ce8bf4f66c71f82a576a6b4c3a17f2e
Ryan McAlpine <mcalpine06@outlook.com>
2023-02-02 20:52:59 +1100

    Finished basic version of set_default_config()

--- src/config.py
+++ src/config.py
@@ -1,9 +1,19 @@
 import configparser
 from os import mkdir
+from os.path import exists
 
 def parse_configs(confdir, conffile):
     return
 
 def set_default_config(confdir, conffile):
-    mkdir(confdir)
+    config = configparser.ConfigParser()
+
+    if not exists(confdir):
+        mkdir(confdir)
+
+    if not exists(conffile):
+        config["Config"] = {"Compression": "zstd"}
+
+    with open(conffile, 'w') as file:
+        config.write(file)
 
