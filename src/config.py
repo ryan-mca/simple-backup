3b0efc6876e48aac5bf1adbf9d8830806d6d74f8
Ryan McAlpine <mcalpine06@outlook.com>
2023-02-02 20:44:31 +1100

    Starting config.py, will be adding support for several compression methods. probably

---
+++ src/config.py
@@ -0,0 +1,9 @@
+import configparser
+from os import mkdir
+
+def parse_configs(confdir, conffile):
+    return
+
+def set_default_config(confdir, conffile):
+    mkdir(confdir)
+
--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
-import configparser
 import tarfile
 import zstandard
+from shutil import copy2
+from appdirs import user_config_dir
+from os import mkdir
+from os.path import exists
+
+CONFDIR = user_config_dir("simple-backup", None)
+CONFFILE = f"{CONFDIR}/config"
 
 def main():
     return
