b4f2f8ebd0802349917a8bd929d2b92e72580237
Ryan McAlpine <mcalpine06@outlook.com>
2023-02-02 14:45:42 +1100

    Started blank main.py template

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import configparser
+import tarfile
+import zstandard
+
+def main():
+    return
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("CTRL+C")
