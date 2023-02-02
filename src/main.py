d38f6c33499c521d99a3e83c3d2e00941dd32634
Ryan McAlpine <mcalpine06@outlook.com>
2023-02-03 01:03:42 +1100

    Reformatting compress.py because shutil.make_archive sucks ass

--- src/main.py
+++ src/main.py
@@ -6,7 +6,6 @@ import csv_read
 
 CONFDIR = user_config_dir("simple-backup", None)
 CONFFILE = f"{CONFDIR}/files.csv"
-COMP_METHODS = ["zstd", "zip", "gzip", "xz", "bzip", "lz4", "tar", "none"]
 
 def main():
     parser = arg_parse.start_argparse()
@@ -16,17 +15,6 @@ def main():
     orig_file = args.Original
     # Removes the extra file extension as without it would create NAME.zip.zip
     dest_file = args.Destination.replace(f".{comp_method}", "").replace(".tar", "")
-
-    # Loop through the COMP_METHODS list and create the corresponding archive
-    if comp_method in COMP_METHODS:
-        func = getattr(compress, f"create_{comp_method}")
-        func(dest_file, orig_file)
-    elif comp_method == "none":
-        comp_method.copy(dest_file, orig_file)
-    else:
-        exit("Compression mode not supported")
-
-
 
 if __name__ == "__main__":
     try:
